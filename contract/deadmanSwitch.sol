292928204d4af08dff9581c22fcdea06bdb50762
Joe <joew@joew.net>
2018-05-28 21:59:22 -0400

    comments

--- contract/deadmanSwitch.sol
+++ contract/deadmanSwitch.sol
@@ -1,50 +1,47 @@
 pragma solidity ^0.4.23;
 
-/** Deadman Switch Trustfund: Allow withdraw of funds if checkin function not executed within timeframe
- *
- * @dev Currently testing on Ropsten
- *
- */
+// Deadman Switch Trustfund: Allow withdraw of funds if checkin function not executed within timeframe
 
-/**
- * @title deadmanSwitch
- * @dev ...
- */
+// @title deadmanSwitch
 contract deadmanSwitch {
   address public owner = msg.sender;
-  //ACCOUNT #2
-  address public beneficiary = 0xC4d1a08E7F6E6211ffea4B35Aa685a8BE249ECca;
-  uint constant year = 31556926;      // Seconds in year
-  uint public min_time = 1545739200;  // Tuesday, December 25, 2018 7:00:00 AM GMT-05:00
 
-  /*
-   * @dev set to true on initial deploy
-   */
+  // ACCOUNT #2
+  address public beneficiary = 0x3f1e38f7c085ca03a5ff35f1c354b4fc16645b32;
+
+  // @dev Seconds in year
+  uint constant year = 31556926;
+
+  // @dev Tuesday, December 25, 2018 7:00:00 AM GMT-05:00
+  uint public min_time = 1545739200;
+
+  // @dev set to true on initial deploy
   bool public alive = true;
-//  uint public balance = 0;
+  uint public balance = 0;
 
-  /*
-   * @devConfirm alive (should be once a year, but owner can extend indefinitely)
-   */
+  // @dev Confirm alive (should be once a year, but owner can extend indefinitely)
   uint public checkins = 0;
 
-  // Events
-  event CheckIn(address who);     // Log CheckIns
-  event CheckAlive(address who);  // Log Queries for alive status
+  // @dev Log CheckIns
+  event CheckIn(address who);
+  // @dev Log Queries for alive status
+  event CheckAlive(address who);
 
 
   function checkin() public returns (bool) {
     if (msg.sender == owner || msg.sender == beneficiary) {
       alive = true;
-      min_time += year; // Add 1 year to min withdraw time
+      // @dev Add 1 year to min withdraw time
+      min_time += year;
       checkins++;
-      emit CheckIn(msg.sender); // logging event
+      // @dev logging event
+      emit CheckIn(msg.sender);
       return true;
     }
     return false;
   }
 
-  // Need to set alive status before withdrawing funds
+  // @dev Need to set alive status before withdrawing funds
   function checkAlive() public returns (bool) {
     if (block.timestamp > min_time) {
       alive = false; // RIP
@@ -54,21 +51,19 @@ contract deadmanSwitch {
   return true;
   }
 
-  // Set new owner
+  // @dev Set new owner
   function transferOwnership(address newOwner) public {
     require(newOwner != address(0) && owner == msg.sender);
     owner = newOwner;
   }
 
-  /* @dev Allow owner to set new beneficiary
-   * @params
-   */
+  // @dev Allow owner to set new beneficiary
   function transferBeneficiary(address newBeneficiary) public {
     require(newBeneficiary != address(0) && owner == msg.sender);
     beneficiary = newBeneficiary;
   }
 
-  // Beneficiary can withdraw funds if owner doesn't check in (checked out)
+  // @dev Beneficiary can withdraw funds if owner doesn't check in (checked out)
   function withdraw() public {
     require (block.timestamp > min_time && alive != true);
     if (msg.sender == owner || msg.sender == beneficiary) {
@@ -77,6 +72,6 @@ contract deadmanSwitch {
   }
 
   function () public payable {
-  //  balance = msg.value;
+    balance = msg.value;
   }
 }
