206913aacf1c4f28348a0fe2d95702df2b822e4a
Joe <joew@joew.net>
2018-08-15 01:38:20 -0400

    test

--- test/TestDeadmanSwitch.js
+++ test/TestDeadmanSwitch.js
@@ -21,13 +21,14 @@ contract('DeadmanSwitch', function (accounts) {
 
     function assertJump(error) {
           if(error.toString().indexOf("invalid JUMP") == -1) {
-            console.log("We were expecting a Solidity throw (aka an invalid JUMP)," + 
+            console.log("We were expecting a Solidity throw (aka an invalid JUMP)," +
                         "we got one. Test succeeded.");
         } else {
             assert(false, error.toString());
         }
     }
 
+  /* owner should equal accounts[0] */
   it("should return the owner", function () {
     return DeadmanSwitch.deployed().then(function (instance) {
       return instance.owner.call();
@@ -36,6 +37,7 @@ contract('DeadmanSwitch', function (accounts) {
     });
   });
 
+  /* check that the beneficiary is set to 0xf46a36B1EAfDa5F7597395EEAB4c89992F851308 */
   it("should return 0xf46...308 as the beneficary", function () {
     return DeadmanSwitch.deployed().then(function (instance) {
       return instance.beneficiary.call();
@@ -44,6 +46,7 @@ contract('DeadmanSwitch', function (accounts) {
       });
   });
 
+  /* check that the initial allowed withdraw time is equal to December 25, 2018 */
   it("check seconds in min_time is equal to 1545739200", function () {
     return DeadmanSwitch.deployed().then(function (instance) {
       return instance.min_time.call();
@@ -52,6 +55,7 @@ contract('DeadmanSwitch', function (accounts) {
       });
   });
 
+  /* sanity check: ensure that 1 year is 31556926 seconds */
   it("check seconds in year is equal to 31556926", function () {
     return DeadmanSwitch.deployed().then(function (instance) {
       return instance.year.call();
@@ -60,28 +64,30 @@ contract('DeadmanSwitch', function (accounts) {
       });
   });
 
+  /* do not allow transferring the beneficiary address to 0x0 */
   it("ensure that transferring the beneficary to null is rejected", function () {
     return DeadmanSwitch.deployed().then(function (instance) {
       return expectThrow(instance.transferBeneficiary(0x0, { from: accounts[0] }));
     });
   });
 
+  /* check that withdraws work for owner */
   it("should allow withdrawal from owner", function () {
     return DeadmanSwitch.deployed().then(function (instance) {
       return expectThrow(instance.withdraw({ from: accounts[0] }));
     });
   });
 
-    // Test pause()
-    it("dont allow withdraws if contract is paused.", async() => {
-        const deadmanSwitch = await DeadmanSwitch.deployed();
-        await deadmanSwitch.pause(); //from defaultAccount -> the owner
+  /* test the circuit breaker feature. pauses should not allow withdraws */
+  it("dont allow withdraws if contract is paused.", async() => {
+      const deadmanSwitch = await DeadmanSwitch.deployed();
+      await deadmanSwitch.pause(); //from defaultAccount -> the owner
 
-        try {
-            await deadmanSwitch.withdraw("Qmm..", {from: accounts[0], value: price});
-            assert.fail('should have thrown before');
-        } catch(error) {              
-            assertJump(error);
-        }    
-    })
+      try {
+          await deadmanSwitch.withdraw("Qmm..", {from: accounts[0], value: price});
+          assert.fail('should have thrown before');
+      } catch(error) {
+          assertJump(error);
+      }
+  })
 });
